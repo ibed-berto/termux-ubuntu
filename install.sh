e9c77b960aa3ccebed84e28fc1dd346cad7db8a7
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-07-01 01:57:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ pkg update && pkg upgrade -y
 pkg install proot -y
 pkg install wget -y
 clear
-echo "$(tput setaf 6)"
+
 read -p "Masukan versi ubuntu yang ingin anda install ?
-$(tput setaf 3)
+
 1) v20.04.2
 
 2) v20.10
@@ -17,7 +17,6 @@ $(tput setaf 3)
 if [ "$versi" = "1" ] || [ "$versi" = "20.04.2" ] || [ "$versi" = "20.04" ];
 then
 
-echo "$(tput sgr 0)"
 wget https://raw.githubusercontent.com/ibed-berto/termux-ubuntu/main/ubuntu-v20.04.sh && chmod +x ubuntu-v20.04.sh && ./ubuntu-v20.04.sh
 
 elif [ "$versi" = "2" ] || [ "$versi" = "20.10" ];
@@ -29,13 +28,12 @@ elif [ "$versi" = "3" ] || [ "$versi" = "21" ];
 then
 
 wget https://raw.githubusercontent.com/ibed-berto/termux-ubuntu/main/ubuntu-v21.sh && chmod +x ubuntu-v21.sh && ./ubuntu-v21.sh
-echo "$(tput sgr 0) "
+
 fi
 
 if [ ! "$versi" = "1" ] || [ ! "$versi" = "20.04.2" ] || [ ! "$versi" = "20.04" ] || [ ! "$versi" = "3" ] || [ ! "$versi" = "21" ] || [ ! "$versi" = "2" ] || [ ! "$versi" = "20.10" ]; 
 then
- echo "$(tput setaf 6)
-Anda salah memasukan format. Masukan format sesuai pilihan di atas yang benar. 
-$(tput sgr 0)"
+clear
+echo "Anda salah memasukan format. Masukan format sesuai pilihan di atas yang benar. "
 
 fi
