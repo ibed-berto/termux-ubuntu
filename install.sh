71b6b2aa15010cf3ce881baf7cf7e784c24ae2ae
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-07-01 12:09:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,21 +35,20 @@ if [ "$versi" = "1" ] || [ "$versi" = "20.04.2" ] || [ "$versi" = "20.04" ];
 then
 
 wget https://raw.githubusercontent.com/ibed-berto/termux-ubuntu/main/ubuntu-v20.04.sh && chmod +x ubuntu-v20.04.sh && ./ubuntu-v20.04.sh
-
+exit
 elif [ "$versi" = "2" ] || [ "$versi" = "20.10" ];
 then
 
 wget https://raw.githubusercontent.com/ibed-berto/termux-ubuntu/main/ubuntu-v20.10.sh && chmod +x ubuntu-v20.10.sh && ./ubuntu-v20.10.sh
-
+exit
 elif [ "$versi" = "3" ] || [ "$versi" = "21" ];
 then
 
 wget https://raw.githubusercontent.com/ibed-berto/termux-ubuntu/main/ubuntu-v21.sh && chmod +x ubuntu-v21.sh && ./ubuntu-v21.sh
-
-fi
-fi
-
 exit
+fi
+fi
+
 
 if [ ! "$versi" = "1" ] || [ ! "$versi" = "20.04.2" ] || [ ! "$versi" = "20.04" ] || [ ! "$versi" = "3" ] || [ ! "$versi" = "21" ] || [ ! "$versi" = "2" ] || [ ! "$versi" = "20.10" ]; 
 then
