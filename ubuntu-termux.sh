1435440d8c09fdcf40cf6311ce184b1740c1d5ab
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-07-04 12:16:33 +0700

    Update ubuntu-termux.sh

--- ubuntu-termux.sh
+++ ubuntu-termux.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 
-/data/data/com.termux/files/home/ubuntu-termux.sh                                                                                                     1403/1403              100%
-#!/bin/bash
-
 clear
 cek=/data/data/com.termux/files
 
 if [ -f "$ada" ] || [ -f "/etc/apt/sources.list" ];
 then
-echo "
+echo -e "\x1b[32m\033[1m
 Sudah ada ubuntu yang terinstall di perangkat anda.
 Silahkan hapus atau install ulang untuk melanjutkan.
 "
@@ -17,23 +14,23 @@ exit
 fi
 
 
-     pkg update && pkg upgrade -y
-echo " [ Sedang mempersiapkan file pelengkap. ] "
+pkg update && pkg upgrade -y
+echo -e "\x1b[32m\033[1mSedang mempersiapkan file pelengkap. "
 if [ ! -f "$cek/usr/bin/wget" ] ; then
-         echo -e "\x1b[32m [ Sedang menginstall wget ] " && sleep 1
+         echo -e "\x1b[32m\033[1mSedang menginstall wget" && sleep 1
          pkg install wget -y && sleep 1
 fi
 
 if [ ! -f "$cek/usr/bin/proot" ] ; then
-         echo -e "\x1b[32m [ Sedang menginstall proot ] " && sleep 1
+         echo -e "\x1b[32mSedang menginstall proot " && sleep 1
          pkg install proot -y && sleep 1
 fi
 
 CPU=$(dpkg --print-architecture)
 case "$CPU" in
-aarch64) ARCHITECTURE=arm64;;
-arm) ARCHITECTURE=armhf;;
-amd64|x86_64) ARCHITECTURE=amd64;;
+aarch64) CPU=arm64;;
+arm) CPU=armhf;;
+amd64|x86_64) CPU=amd64;;
 *)
 echo -e "\x1b[32m\033[1m [ERROR]: Architecture tidak terdeteksi :- $CPU"
 exit 1
@@ -71,10 +68,10 @@ read -p "Silahkan masukan jenis varian ubuntu versi 12.04 yang ingin anda instal
 
 if  [ "$satu" = "1" ] || [ "$satu" = "12.04.4" ];
 then
-wget "https://cdimage.ubuntu.com/ubuntu-base/releases/12.04/release/ubuntu-base-12.04.4-core-$CPU.tar.gz -q -O ubuntu.tar.gz "
+wget https://cdimage.ubuntu.com/ubuntu-base/releases/12.04/release/ubuntu-base-12.04.4-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
 elif [ "$satu"  = "2" ] || [ "satu"  = "12.04.5" ];
 then
-wget "https://cdimage.ubuntu.com/ubuntu-base/releases/12.04/release/ubuntu-base-12.04.5-core-$CPU.tar.gz -q -O ubuntu.tar.gz  "
+wget https://cdimage.ubuntu.com/ubuntu-base/releases/12.04/release/ubuntu-base-12.04.5-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
 fi
 fi
 
@@ -98,10 +95,10 @@ read -p "Silahkan masukan jenis varian ubuntu versi 14.04 yang ingin anda instal
 
 if  [ "$dua" = "1" ] || [ "$dua" = "14.04" ];
 then
-wget "https://cdimage.ubuntu.com/ubuntu-base/releases/14.04/release/ubuntu-base-14.04-core-$CPU.tar.gz -q -O ubuntu.tar.gz "
+wget https://cdimage.ubuntu.com/ubuntu-base/releases/14.04/release/ubuntu-base-14.04-core-$CPU.tar.gz -q -O ubuntu.tar.gz
 elif [ "$dua"  = "2" ] || [ "$dua"  = "14.04.1" ];
 then
-wget "https://cdimage.ubuntu.com/ubuntu-base/releases/14.04/release/ubuntu-base-14.04.1-core-$CPU.tar.gz -q -O ubuntu.tar.gz  "
+wget https://cdimage.ubuntu.com/ubuntu-base/releases/14.04/release/ubuntu-base-14.04.1-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
 fi
 fi
 
