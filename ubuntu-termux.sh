792911b1103de8aa695aa1b56039b969aed6e99a
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2021-07-04 21:20:50 +0700

    Update ubuntu-termux.sh

--- ubuntu-termux.sh
+++ ubuntu-termux.sh
@@ -41,47 +41,60 @@ esac
 clear
 dir=$(pwd)
 if [ ! -f "$ada" ] || [ ! -f "/etc/apt/sources.list" ];
-then
-
-read -p "Pilih versi ubuntu yang ingin anda install ?
-1) Versi 12.04          4) Versi 18.04          7) Versi 21.04
-2) Versi 14.04          5) Versi 20.04
-3) Versi 16.04          6) Versi 20.10
-" versi
-
+   then
+   
+   ubuntu="Versi-12.04 Versi-14.04 Versi-16.04 Versi-18.04 Versi-20.04 Versi-20.10 Versi-21.04"
+   echo -e  "\x1b[32m\033[1mSilahkan masukan versi ubuntu yang ingin anda install : \n"
+   select versi in $ubuntu
+   do
+       if [ "$versi" == "Versi-21.04" ];
+       then 
+       break
+       fi           
+             satu="Versi-12.04.04 Versi-12.04.05"
+             echo -e "\x1b[32m\033[1mBerikut ini untuk varian ubuntu $versi : \n"
+             select abs in $satu
+                  do
+                       if [ "$abs" == "Versi-12.04.05" ];
+                       then
+                       break
+                       fi
+                       echo "Helo $abs"
+                  done
+       done
 
 clear
 
-if [ "$versi" = "1" ] || [ "$versi" = "12.04" ] || [ "$versi" = "12" ];
-then
+   if [ "$versi" = "1" ] || [ "$versi" = "12.04" ] || [ "$versi" = "12" ];
+   then
 
-echo -e "\x1b[32m\033[1mBerikut ini untuk varian ubuntu versi 12.04 : "
-echo -e "\x1b[32m\033[1m 
-1) Versi 12.04.4
+   echo -e "\x1b[32m\033[1mBerikut ini untuk varian ubuntu versi 12.04 : "
+   echo -e "\x1b[32m\033[1m 
+   1) Versi 12.04.4
 
-2) Versi 12.04.5
+   2) Versi 12.04.5
 "
 
 
-read -p "Silahkan masukan jenis varian ubuntu versi 12.04 yang ingin anda install : 
+   read -p "Silahkan masukan jenis varian ubuntu versi 12.04 yang ingin anda install : 
 " satu
 
-if  [ "$satu" = "1" ] || [ "$satu" = "12.04.4" ];
-then
-wget https://cdimage.ubuntu.com/ubuntu-base/releases/12.04/release/ubuntu-base-12.04.4-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
-elif [ "$satu"  = "2" ] || [ "satu"  = "12.04.5" ];
-then
-wget https://cdimage.ubuntu.com/ubuntu-base/releases/12.04/release/ubuntu-base-12.04.5-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
-fi
-fi
+         if  [ "$satu" = "1" ] || [ "$satu" = "12.04.4" ];
+         then
+         wget https://cdimage.ubuntu.com/ubuntu-base/releases/12.04/release/ubuntu-base-12.04.4-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
+         elif [ "$satu"  = "2" ] || [ "satu"  = "12.04.5" ];
+         then
+         wget https://cdimage.ubuntu.com/ubuntu-base/releases/12.04/release/ubuntu-base-12.04.5-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
+         fi
+    fi
 
 clear
 
-if [ "$versi" = "2" ] || [ "$versi" = "14.04" ] || [ "$versi" = "14" ];
-then
+    if [ "$versi" = "2" ] || [ "$versi" = "14.04" ] || [ "$versi" = "14" ];
+    then
 
-echo -e "\x1b[32m\033[1mBerikut ini untuk varian ubuntu versi 14.04 : "
-echo -e "\x1b[32m\033[1m 
+    echo -e "\x1b[32m\033[1mBerikut ini untuk varian ubuntu versi 14.04 : "
+    echo -e "\x1b[32m\033[1m 
 1) Versi 14.04            4) Versi 14.04.3          7) Versi 14.04.6
 
 2) Versi 14.04.1          5) Versi 14.04.4
@@ -90,17 +103,17 @@ echo -e "\x1b[32m\033[1m
 "
 
 
-read -p "Silahkan masukan jenis varian ubuntu versi 14.04 yang ingin anda install : 
+     read -p "Silahkan masukan jenis varian ubuntu versi 14.04 yang ingin anda install : 
 " dua
 
-if  [ "$dua" = "1" ] || [ "$dua" = "14.04" ];
-then
-wget https://cdimage.ubuntu.com/ubuntu-base/releases/14.04/release/ubuntu-base-14.04-core-$CPU.tar.gz -q -O ubuntu.tar.gz
-elif [ "$dua"  = "2" ] || [ "$dua"  = "14.04.1" ];
-then
-wget https://cdimage.ubuntu.com/ubuntu-base/releases/14.04/release/ubuntu-base-14.04.1-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
-fi
-fi
+           if  [ "$dua" = "1" ] || [ "$dua" = "14.04" ];
+           then
+           wget https://cdimage.ubuntu.com/ubuntu-base/releases/14.04/release/ubuntu-base-14.04-core-$CPU.tar.gz -q -O ubuntu.tar.gz
+           elif [ "$dua"  = "2" ] || [ "$dua"  = "14.04.1" ];
+           then
+           wget https://cdimage.ubuntu.com/ubuntu-base/releases/14.04/release/ubuntu-base-14.04.1-core-$CPU.tar.gz -q -O ubuntu.tar.gz 
+           fi
+     fi
 
 fi #ada
 
